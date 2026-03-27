3575697efe8ef0e9e4a6743bf338e68a9befdd3b
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-27 11:41:52 -0400

    Create Helper Function for music playback

--- Assets/Scripts/Handlers/AudioHandler.cs
+++ Assets/Scripts/Handlers/AudioHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using Mono.Cecil;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -22,7 +24,7 @@ public class AudioHandler : MonoBehaviour
         musicSource = gameObject.AddComponent<AudioSource>();
         musicSource.clip = musicClip;
         
-        musicSource.loop = false; // this needs to be here to override the default, otherwise unity will loop from the start regardless
+        
     }
     private void Start()
     {
@@ -31,17 +33,38 @@ public class AudioHandler : MonoBehaviour
         // subtract the amount of samples in the clip by the frequency divided by framerate. This calculates how many samples were passed in a given frame, to avoid the loop check to pass the clip length before the next frame.
         clipLength = musicSource.clip.samples - Mathf.CeilToInt(musicSource.clip.frequency / Application.targetFrameRate);
 
-        musicSource.Play();
     }
 
     private void Update()
     {
 
         currentPosition = musicSource.timeSamples; // for debug purposes
-        
-        if (musicSource.timeSamples >= clipLength) 
+        if (musicSource == null)
+        {
+            Debug.LogError("Error! musicSource not found");
+            return;
+        }
+        else if (musicClip == null)
+        {
+            Debug.LogError("Error! Clip not assigned to AudioSource Component. Audio will not play.");
+        }
+        else if (musicSource.timeSamples >= clipLength) 
         { 
             musicSource.timeSamples = loopStartSamples;
         }
+    }
+
+    private void PlayMusic(String path, float loopPoint)
+    {
+        musicSource.loop = false; // this needs to be here to override the default, otherwise unity will loop from the start regardless
+
+        musicClip = Resources.Load<AudioClip>(path);
+        loopStart = loopPoint;
+        musicSource.Play();
+    }
+
+    private void PlaySFX(string path)
+    {
+        // TODO: SFX Handling
     }
 }
