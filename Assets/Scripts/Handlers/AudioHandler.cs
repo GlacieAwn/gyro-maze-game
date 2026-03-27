503ba099e7755aabc74c55bee17f4a73f25287e8
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-27 11:20:55 -0400

    Updated audio script to not require an AudioSource component

--- Assets/Scripts/Handlers/AudioHandler.cs
+++ Assets/Scripts/Handlers/AudioHandler.cs
@@ -1,40 +1,47 @@
-using Mono.Cecil.Cil;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
+// [RequireComponent(typeof(musicSource))]
 public class AudioHandler : MonoBehaviour
 {
     [Tooltip("Loop point in seconds")]
     [SerializeField] private float loopStart = 0f;
     [SerializeField] private int currentPosition;
 
+    [SerializeField] private AudioClip musicClip;
+    [SerializeField] private AudioClip sfxClip;
+    
     private int loopStartSamples;
     private int clipLength;
 
-    private AudioSource audioSource;
+    private AudioSource musicSource;
 
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>();
-
-        audioSource.loop = false; // this needs to be here to override the default, otherwise unity will loop from the start regardless
+        musicSource = gameObject.AddComponent<AudioSource>();
+        musicSource.clip = musicClip;
+        
+        musicSource.loop = false; // this needs to be here to override the default, otherwise unity will loop from the start regardless
     }
     private void Start()
     {
-        loopStartSamples = Mathf.FloorToInt(loopStart * audioSource.clip.frequency);
+        loopStartSamples = Mathf.FloorToInt(loopStart * musicSource.clip.frequency);
 
         // subtract the amount of samples in the clip by the frequency divided by framerate. This calculates how many samples were passed in a given frame, to avoid the loop check to pass the clip length before the next frame.
-        clipLength = audioSource.clip.samples - Mathf.CeilToInt(audioSource.clip.frequency / Application.targetFrameRate);
+        clipLength = musicSource.clip.samples - Mathf.CeilToInt(musicSource.clip.frequency / Application.targetFrameRate);
+
+        musicSource.Play();
     }
 
     private void Update()
     {
 
-        currentPosition = audioSource.timeSamples; // for debug purposes
+        currentPosition = musicSource.timeSamples; // for debug purposes
         
-        if (audioSource.timeSamples >= clipLength) 
+        if (musicSource.timeSamples >= clipLength) 
         { 
-            audioSource.timeSamples = loopStartSamples;
+            musicSource.timeSamples = loopStartSamples;
         }
     }
 }
